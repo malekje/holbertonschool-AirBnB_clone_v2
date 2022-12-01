99042993322e68dacbd9ba0edaf98560ebd44013
Malek Mayeh <loukatankionline@gmail.com>
2022-12-01 13:25:54 +0100

    number template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -33,7 +33,7 @@ def isitnumber(n):
 
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
-def template(n):
+def n_template(n):
     return render_template('5-number.html', n=n)
 
 if __name__ == '__main__':
